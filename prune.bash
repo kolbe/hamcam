4d13b058f0e7076caabfbeeaa73b3d2cff7b2676
Kolbe Kegel <kolbe@kolbekegel.com>
2022-10-21 17:53:34 -0700

    added prune.bash

---
+++ prune.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+cd /mnt/bucket/bucket/cam/floathouse/lake/AMC0461CEA066DC1D0/ || exit
+for d in 2022-09-*
+do
+	[[ -d $d ]] || continue
+	printf -v url 'https://api.sunrise-sunset.org/json?formatted=0&lat=47.64&lng=-122.33&date=%s' "$d"
+	echo "$d"
+	sunset=$( curl -sS "$url" | jq -r .results.sunset )
+	sunset_hour=$( date -d "$sunset" +%H )
+	for ((h=0; h<24; h++))
+	do
+		(( sunset_hour-1 <= h && h <= sunset_hour+1 )) && continue
+		printf -v prefix %02i "$h"
+		files=("$d"/pic_001/"$prefix"*)
+		unset files[0]
+		echo "$h ${#files[@]}"
+		((${#files[@]})) && rm -v "${files[@]}"
+	done
+done
